9cd1c80edaf2e4483f8441abe6ed0c2580358f9a
Krunal Shambharkar <krunalss@outlook.com>
2024-11-18 07:42:33 +0530

    completed implementaion ready for MLOPS

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'medical_bot',
+    version= '0.0.0',
+    author= 'Bappy Ahmed',
+    author_email= 'entbappy73@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
